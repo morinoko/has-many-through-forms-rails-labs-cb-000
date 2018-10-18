c820326833f1bd5489254a50418d9814b4ad1014
Felice Forby <feliceforby@gmail.com>
2018-10-18 23:45:10 +0000

    build out new post form and show page

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,21 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories, reject_if: :all_blank
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
+  def category_list
+    category_names = self.categories.map { |category| category.name }
+    category_names.join(", ")
+  end
+
+  def comment_count
+    self.comments.count
+  end
 end
