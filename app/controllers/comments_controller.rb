6cc3bfa7d8f459a9742ef03d4ee15a1b9c122f6b
Felice Forby <feliceforby@gmail.com>
2018-10-20 06:02:25 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
 class CommentsController < ApplicationController
-
   def create
     comment = Comment.create(comment_params)
     redirect_to comment.post
+  end
+
+  def user_attributes=(user_attributes)
+    user_attributes.values.each do |user_attribute|
+      user = User.find_or_create_by(user_attribute)
+      self.user = user
+    end
   end
 
   private
--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -13,14 +13,14 @@ describe 'comments', type: 'feature' do
     fill_in('comment_content', with: 'great post!')
     select(@user.username, from: 'comment_user_id')
     click_button('Create Comment')
-    expect(page).to have_content("#{@user.username} says: great post!")
+    expect(page).to have_content("#{@user.username} says:\ngreat post!")
   end
 
   it 'creates a comment and a new user' do
     fill_in('comment_content', with: 'i agree')
     fill_in('comment_user_attributes_username', with: 'CoolPerson25')
     click_button('Create Comment')
-    expect(page).to have_content("CoolPerson25 says: i agree")
+    expect(page).to have_content("CoolPerson25 says:\ni agree")
   end
 
 end
